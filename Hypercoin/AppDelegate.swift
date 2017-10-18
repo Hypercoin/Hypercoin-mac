ad1a0bc77aadfdfe2d57c1855db56d16886b7d88
Benjamin Prieur <benjamin@prieur.org>
2017-10-18 12:20:58 +0200

    initial commit

---
+++ Hypercoin/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Hypercoin
+//
+//  Created by Benjamin Prieur on 18/10/2017.
+//  Copyright © 2017 Hypercoin. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ Hypercoin/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Hypercoin
+//
+//  Created by Benjamin Prieur on 18/10/2017.
+//  Copyright © 2017 Hypercoin. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
