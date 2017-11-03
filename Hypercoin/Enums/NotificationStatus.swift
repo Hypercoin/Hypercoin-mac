c47b5e89b4e52700adabc437548f8fa477bf54ab
Ben <benjamin@prieur.org>
2017-11-03 08:59:48 +0100

    Notify when stqtus has changed

---
+++ Hypercoin/Enums/NotificationStatus.swift
@@ -0,0 +1,15 @@
+//
+//  NotificationStatus.swift
+//  Hypercoin
+//
+//  Created by Benjamin Prieur on 03/11/2017.
+//  Copyright © 2017 Hypercoin. All rights reserved.
+//
+
+import Foundation
+
+enum NotifiactionStatus {
+	case up
+	case down
+	case none
+}
