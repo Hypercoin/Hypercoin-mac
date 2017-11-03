f3325b41737c84d2b281b50942d70dadc70358c7
Ben <benjamin@prieur.org>
2017-11-03 08:31:28 +0100

    Add notification and repeat api call every 30s

--- Hypercoin/Models/MarketCap.swift
+++ Hypercoin/Models/MarketCap.swift
@@ -15,8 +15,8 @@ struct MarketCap: Unmarshaling {
 	public var symbol: String
 	public var rank: Int
 	public var price: [CurrencyType: Double]
-    public var volume24h: [CurrencyType: Double]
-    public var marketCap: [CurrencyType: Double]
+	public var volume24h: [CurrencyType: Double]
+	public var marketCap: [CurrencyType: Double]
 	public var availableSupply: Double
 	public var totalSupply: Double
 	public var percentChange: [ChangePeriodType: Double]
@@ -25,8 +25,8 @@ struct MarketCap: Unmarshaling {
 	init(object: MarshaledObject) throws {
 		self.price = [:]
 		self.percentChange = [:]
-        self.volume24h = [:]
-        self.marketCap = [:]
+		self.volume24h = [:]
+		self.marketCap = [:]
 
 		self.id = try object.value(for: "id")
 		self.name = try object.value(for: "name")
@@ -36,24 +36,24 @@ struct MarketCap: Unmarshaling {
 		self.totalSupply = try object.double(for: "total_supply", defaultValue: 0)
 		self.lastUpdated = try object.value(for: "last_updated", defaultValue: 0)
 
-        for currency in CurrencyType.allValues {
-            if let price = try? object.double(for: "price_\(currency)", defaultValue: 0), price > 0 {
-                self.price[currency] = price
-            }
+		for currency in CurrencyType.allValues {
+			if let price = try? object.double(for: "price_\(currency)", defaultValue: 0), price > 0 {
+				self.price[currency] = price
+			}
 
-            if let price = try? object.double(for: "24h_volume_\(currency)", defaultValue: 0), price > 0 {
-                self.volume24h[currency] = price
-            }
+			if let price = try? object.double(for: "24h_volume_\(currency)", defaultValue: 0), price > 0 {
+				self.volume24h[currency] = price
+			}
 
-            if let price = try? object.double(for: "market_cap_\(currency)", defaultValue: 0), price > 0 {
-                self.marketCap[currency] = price
-            }
-        }
+			if let price = try? object.double(for: "market_cap_\(currency)", defaultValue: 0), price > 0 {
+				self.marketCap[currency] = price
+			}
+		}
 
 
-        self.percentChange[.hourly] = try object.double(for: "percent_change_1h", defaultValue: 0)
-        self.percentChange[.daily] = try object.double(for: "percent_change_24h", defaultValue: 0)
-        self.percentChange[.weekly] = try object.double(for: "percent_change_7d", defaultValue: 0)
+		self.percentChange[.hourly] = try object.double(for: "percent_change_1h", defaultValue: 0)
+		self.percentChange[.daily] = try object.double(for: "percent_change_24h", defaultValue: 0)
+		self.percentChange[.weekly] = try object.double(for: "percent_change_7d", defaultValue: 0)
 	}
 }
 
